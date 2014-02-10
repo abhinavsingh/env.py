42b29ddd842e6b9857bc587b65db9e09e0db3e05
Abhinav Singh <mailsforabhinav@gmail.com>
2014-02-11 03:02:33 +0530

    fix when used from non package

--- env.py
+++ env.py
@@ -1,13 +1,14 @@
-import os
-from importlib import import_module
-
-VERSION = (0, 1)
+VERSION = (0, 2)
 __version__ = '.'.join(map(str, VERSION[0:2]))
 __description__ = 'Easily manage your python application settings for different environments'
 __author__ = 'Abhinav Singh'
 __author_email__ = 'mailsforabhinav@gmail.com'
 __homepage__ = 'https://github.com/abhinavsingh/env.py'
 __license__ = 'BSD'
+
+import os
+import sys
+from importlib import import_module
 
 try:
     from django.core.exceptions import ImproperlyConfigured
@@ -17,13 +18,19 @@ except:
 
 ENV = os.environ.get('SETTINGS_ENV')
 if ENV is None:
+    importer = sys._getframe(1).f_globals.get('__name__')
     try:
-        from local_env import ENV
-    except:
+        if not importer:
+            from local_env import ENV
+        else:
+            parts = importer.split('.')
+            parts = parts[:-1] if len(parts) > 1 else parts
+            ENV = getattr(import_module('%s.local_env' % '.'.join(parts)), 'ENV')
+    except Exception, e:
         raise ENVException('Define ENV variable inside local_env.py or SETTINGS_ENV environment variable')
 
 try:
-    settings = import_module('%s_settings' % ENV)
+    settings = import_module('%s_settings' % ENV if not importer else '%s.%s_settings' % (importer, ENV))
     for k in dir(settings):
         if not k.startswith('__'):
             globals()[k] = getattr(settings, k)
