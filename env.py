a9a7f79b495ba159221b13b382a00bec16187b3e
Abhinav Singh <mailsforabhinav@gmail.com>
2014-02-13 18:57:42 +0530

    cleanup prints

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
-VERSION = (0, 2, 1)
+VERSION = (0, 2, 2)
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 __description__ = 'Easily manage your python application settings for different environments'
 __author__ = 'Abhinav Singh'
@@ -27,13 +27,12 @@ if ENV is None:
             parts = importer.split('.')
             parts = parts[:-1] if len(parts) > 1 else parts
             importer = '.'.join(parts)
-            print 'Importer: %s' % importer
             ENV = getattr(import_module('%s.local_env' % importer), 'ENV')
     except Exception, e:
         raise ENVException('Define ENV variable inside local_env.py or SETTINGS_ENV environment variable')
 
 try:
-    print 'using ENV: %s, %s_settings must exist' % (ENV, ENV)
+    print 'ENV: %s_settings' % ENV
     settings = import_module('%s_settings' % ENV if not importer else '%s.%s_settings' % (importer, ENV))
     for k in dir(settings):
         if not k.startswith('__'):
